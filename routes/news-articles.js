e54f8c4de6f61b37ed3cefc5318bead8a6cea99e
Rikesh Makwana <rikesh.makwana@gmail.com>
2020-05-27 13:25:20 +0400

    fix(news): return no similar articles if source does not have a id

--- routes/news-articles.js
+++ routes/news-articles.js
@@ -23,8 +23,10 @@ router.get('/:articleID', async (req, res, next) => {
   // Get news article from database
   const newsArticle = (await getNewsArticle(articleID)).val();
 
+  const newsSource = newsArticle.source.id;
+
   // Get similar articles from database
-  const similarArticles = (await getNewsArticlesBySource(newsArticle.source.id)).val();
+  const similarArticles = newsSource ? (await getNewsArticlesBySource(newsSource)).val() : {};
 
   // Remove current article from the similar articles
   delete similarArticles[articleID];
