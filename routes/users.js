d5bb12a8104c61cd2978033ad0dbe4ce10ffde0d
Rikesh Makwana <rikesh.makwana@gmail.com>
2020-05-27 14:45:33 +0400

    feat(users): add user profile page with user settings

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/** GET : View user profile */
+router.get('/:userID', function(req, res, next) {
+  // Get user ID from request params
+  const { userID } = req.params;
+
+  // Get the user information from the database
+  // TODO: Define user in the database
+  const user = {
+    name: "John Thomas",
+    role: "Reader",
+    profilePicture: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+  }
+
+  res.render('users/read', {
+    title: "User Profile",
+    user
+  });
 });
 
 module.exports = router;
